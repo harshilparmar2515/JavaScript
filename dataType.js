96cc36f103b32adc0372e174f45a78288e784764
harshilparmar2515 <harshilparmar2515@gmail.com>
2026-01-30 00:46:57 +0530

    Add files via upload

---
+++ dataType.js
@@ -0,0 +1,32 @@
+let a = 10;
+console.log(a)
+console.log(typeof a)
+
+let b = "Harshill..!";
+console.log(b)
+console.log(typeof b)
+
+let c = true;
+console.log(c)
+console.log(typeof c)
+
+let d = 10230948384955555555555932n;
+console.log(d)
+console.log(typeof d)
+
+let f = undefined;
+console.log(f)
+console.log(typeof f)
+
+let g = Symbol(10);
+let h = Symbol(10);
+console.log(g == f)
+console.log(f)
+
+let z = null
+console.log( typeof z)
+
+const array = {Name:"Harshil", Age:18, City:"Bhavnagar"}
+console.log(array)
+
+
